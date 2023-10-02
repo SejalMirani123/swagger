e5542ef52b3f6f0379d069a1f1f7f3917e186913
SejalMirani123 <67837837+SejalMirani123@users.noreply.github.com>
2023-10-02 13:57:06 +0530

    Add files via upload

---
+++ swagger-spring boot3 demo/src/test/java/com/example/swaggerspring/SwaggerSpringBoot3DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swaggerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerSpringBoot3DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
